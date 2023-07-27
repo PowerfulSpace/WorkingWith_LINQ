791f3355169bb034271e94c66c3005129d6b6d09
Alexander <sany199574@gmail.com>
2023-07-27 23:25:06 +0300

    Добавил - StartWithALetter - FindOutAge

---
+++ StartWithALetter/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+string[] people = { "Tom", "Bob", "Sam", "Tim", "Tomas", "Bill" };
+
+// создаем новый список для результатов
+var selectedPeople = from p in people // передаем каждый элемент из people в переменную p
+                     where p.ToUpper().StartsWith("T") //фильтрация по критерию
+                     orderby p  // упорядочиваем по возрастанию
+                     select p; // выбираем объект в создаваемую коллекцию
+
+foreach (string person in selectedPeople)
+    Console.WriteLine(person);
+
+
+Console.ReadLine();
