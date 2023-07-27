bd55603f2e7624fa3e78800ed96fc1cb595841dc
Alexander <sany199574@gmail.com>
2023-07-27 23:06:50 +0300

    Добавьте файлы проекта.

---
+++ Testing/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+string[] people = { "Tom", "Bob", "Sam", "Tim", "Tomas", "Bill" };
+
+// создаем новый список для результатов
+var selectedPeople = new List<string>();
+// проходим по массиву
+foreach (string person in people)
+{
+    // если строка начинается на букву T, добавляем в список
+    if (person.ToUpper().StartsWith("T"))
+        selectedPeople.Add(person);
+}
+// сортируем список
+selectedPeople.Sort();
+
+foreach (string person in selectedPeople)
+    Console.WriteLine(person);
