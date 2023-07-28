4438f72d36c38af43a2588ae3d4f3c21cdfca570
Alexander <sany199574@gmail.com>
2023-07-28 19:20:36 +0300

    Добавил примеры для сортровки, Объединение, пересечение и разность коллекций. Так же и для более сложных обьектов

---
+++ LINQ_Distinct/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+
+
+string[] soft = { "Microsoft", "Google", "Apple", "Microsoft", "Google" };
+
+// удаление дублей
+var result = soft.Distinct();
+
+foreach (string s in result)
+    Console.WriteLine(s);
+
+Console.ReadLine();
+
