f5692ac72725ff518e937af8800b96c69aab99f5
Alexander <sany199574@gmail.com>
2023-07-28 04:50:19 +0300

    Добавил примеры на фильтрацию

---
+++ LINQWhere/Program.cs
@@ -0,0 +1,32 @@
+﻿
+var people = new List<Person>
+{
+    new Person ("Tom", 23, new List<string> {"english", "german"}),
+    new Person ("Bob", 27, new List<string> {"english", "french" }),
+    new Person ("Sam", 29, new List<string>  { "english", "spanish" }),
+    new Person ("Alice", 24, new List<string> {"spanish", "german" })
+};
+
+
+var selectedPeople = from person in people
+                     from lang in person.Languages
+                     where person.Age < 28
+                     where lang == "english"
+                     select person;
+
+foreach (var item in selectedPeople)
+{
+    Console.WriteLine(item.Name + " " + item.Age);
+    foreach (var item2 in item.Languages)
+    {
+        Console.WriteLine(item2);
+    }
+    Console.WriteLine();
+}
+
+
+Console.ReadLine();
+
+
+
+record class Person(string Name, int Age, List<string> Languages);
---
+++ LINQWhereSelectMany/Program.cs
@@ -0,0 +1,30 @@
+﻿
+var people = new List<Person>
+{
+    new Person ("Tom", 23, new List<string> {"english", "german"}),
+    new Person ("Bob", 27, new List<string> {"english", "french" }),
+    new Person ("Sam", 29, new List<string>  { "english", "spanish" }),
+    new Person ("Alice", 24, new List<string> {"spanish", "german" })
+};
+
+
+var selectedPeople = people.SelectMany(u => u.Languages, (u, l) => new { Person = u, Lang = l })
+                          .Where(u => u.Lang == "english" && u.Person.Age < 28)
+                          .Select(u => u.Person);
+
+foreach (var item in selectedPeople)
+{
+    Console.WriteLine(item.Name);
+    foreach (var item2 in item.Languages)
+    {
+        Console.Write("- {0} ", item2);
+    }
+    Console.WriteLine();
+}
+
+
+Console.ReadLine();
+
+
+
+record class Person(string Name, int Age, List<string> Languages);
