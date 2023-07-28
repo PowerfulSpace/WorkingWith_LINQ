e8c958199bc2eb71d4b1bc39dd358953a5cea28d
Alexander <sany199574@gmail.com>
2023-07-28 08:39:10 +0300

    Добавил пару примеров по сортировке

---
+++ LINQOrderByAndThenBy/Program.cs
@@ -0,0 +1,24 @@
+﻿
+var people = new List<Person>
+{
+    new Person("Tom", 37),
+    new Person("Sam", 28),
+    new Person("Tom", 22),
+    new Person("Bob", 41),
+};
+// с помощью оператора orderby
+var sortedPeople1 = from p in people
+                    orderby p.Name, p.Age
+                    select p;
+
+foreach (var p in sortedPeople1)
+    Console.WriteLine($"{p.Name} - {p.Age}");
+
+//С помощи метода расширения
+var sortedPeople2 = people.OrderBy(p => p.Name).ThenByDescending(p => p.Age);
+
+Console.ReadLine();
+
+
+
+record class Person(string Name, int Age);
