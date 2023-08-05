75f0532520d2ca1b6d577b3aaa54e498ed10c87d
Alexander <sany199574@gmail.com>
2023-08-05 13:31:19 +0300

    Добавлен примерна перегрузку методов расширения

---
+++ SortByCharacterLength/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+
+var companyNames = new[] {
+    "Consolidated Messenger", "Alpine Ski House", "Southridge Video",
+    "City Power & Light", "Coho Winery", "Wide World Importers",
+    "Graphic Design Institute", "Adventure Works", "Humongous Insurance",
+    "Woodgrove Bank", "Margie's Travel", "Northwind Traders",
+    "Blue Yonder Airlines", "Trey Research", "The Phone Company",
+    "Wingtip Toys", "Lucerne Publishing", "Fourth Coffee"
+};
+
+IQueryable<string> companyNamesSource = companyNames.AsQueryable();
+var fixedQry = companyNames.OrderBy(x => x);
+
+//Выбор первых символов слов, которые не повторяются
+var length = 1;
+var qry = companyNamesSource
+    .Select(x => x.Substring(0, length))
+    .Distinct();
+
+Console.WriteLine(string.Join(",", qry));
+Console.WriteLine();
+
+length = 2;
+Console.WriteLine(string.Join(",", qry));
+
+
+//Сортировка по длине символов
+qry = companyNamesSource;
+if (qry != null)
+{
+    qry = qry.OrderBy(x => x.Length);
+}
+Console.WriteLine();
+foreach (var item in qry)
+{
+    Console.WriteLine(item);
+}
+
+
+Console.ReadLine();
+
---
+++ SplittingAFileIntoMultipleFilesUsingGroups/Program.cs
@@ -0,0 +1,64 @@
+﻿
+
+
+string[] fileA = File.ReadAllLines(@"D:\Testing\Test.txt");
+string[] fileB = File.ReadAllLines(@"D:\Testing\Test2.txt");
+// Concatenate and remove duplicate names based on  
+// default string comparer  
+var mergeQuery = fileA.Union(fileB);
+
+// Group the names by the first letter in the last name.  
+var groupQuery = from name in mergeQuery
+                 let n = name.Split(',')
+                 group name by n[0][0] into g
+                 orderby g.Key
+                 select g;
+
+foreach (var g in groupQuery)
+{
+    // Create the new file name.  
+    string fileName = @"D:\Testing\NewTesting\" + g.Key + ".txt";
+
+    // Output to display.  
+    Console.WriteLine(g.Key);
+
+    // Write file.  
+    using (StreamWriter sw = new StreamWriter(fileName))
+    {
+        foreach (var item in g)
+        {
+            sw.WriteLine(item);
+            // Output to console for example purposes.  
+            Console.WriteLine("   {0}", item);
+        }
+    }
+}
+
+
+
+Console.ReadLine();
+
+
+
+//Bankov, Peter
+//Holm, Michael  
+//Garcia, Hugo  
+//Potra, Cristina  
+//Noriega, Fabricio  
+//Aw, Kam Foo  
+//Beebe, Ann  
+//Toyoshima, Tim  
+//Guy, Wey Yuan  
+//Garcia, Debra  
+
+
+//Liu, Jinghao
+//Bankov, Peter  
+//Holm, Michael  
+//Garcia, Hugo  
+//Beebe, Ann  
+//Gilchrist, Beth  
+//Myrcha, Jacek  
+//Giakoumakis, Leo  
+//McLin, Nkenge  
+//El Yassir, Mehdi  
